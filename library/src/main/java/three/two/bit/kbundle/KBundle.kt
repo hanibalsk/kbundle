cedb2871063fa92bcb027392dbc077b25a293ab9
Martin Janči <mjanci@cellular.de>
2021-09-28 19:32:57 +0200

    Fixed style

--- library/src/main/java/three/two/bit/kbundle/KBundle.kt
+++ library/src/main/java/three/two/bit/kbundle/KBundle.kt
@@ -1,3 +1,18 @@
+/**
+ * Designed and developed by Aidan Follestad (@afollestad)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package three.two.bit.kbundle
 
 import android.os.Build
@@ -120,7 +135,6 @@ interface BundleBuilder {
 
   /** Key to [Parcelable] list value */
   infix fun String.toParcelableList(value: ArrayList<Parcelable>?)
-
 }
 
 @JvmInline
@@ -256,5 +270,4 @@ internal value class BundleBuilderImpl(val bundle: Bundle = Bundle()) : BundleBu
   override fun String.to(value: SizeF) {
     bundle.putSizeF(this, value)
   }
-
 }
